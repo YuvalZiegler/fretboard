db93ae356a1e9b22de2c4b402ea5bf3e59cab549
YuvalZiegler <yuvalziegler@gmail.com>
2012-08-25 15:33:09 -0400

    pushin

--- public/javascripts/views/fretboard.js
+++ public/javascripts/views/fretboard.js
@@ -1,18 +1,23 @@
 /*global Backbone _ NoteDictionary */
+
 var App = (function (App) {
     App.FretboardView = Backbone.View.extend({
-        el:'#neck',
+        el:'#fretboard',
 
         initialize:function(options){
+            _.bindAll(this, 'render');
+            this.render();
+        },
 
+        render:function(){
             for (var i=0, l = App.stringsCollection.models.length; i<l ; i++ ){
-
                 var string = new App.StringView({model:App.stringsCollection.models[i]});
                 $(this.el).append(string.el);
 
             }
-
         }
+
+
     });
     return App;
 })(App || {});
--- public/javascripts/views/string.js
+++ public/javascripts/views/string.js
@@ -7,26 +7,28 @@ var App = (function (App) {
         className:'string',
         noteModels:[],
 
+
         initialize:function(){
             _.bindAll(this,'render','update');
 
             this.model.bind('change', this.update);
+
             this.render();
+
         },
 
         render: function(){
-
             for (var i= 0; i<12; i++){
                 // creating views for each note
                 var n = new App.NoteView({note:this.model.attributes.octave[i], stringPosition:i});
                 $(this.el).append(n.el);
             }
 
+
         },
         update: function(){
-            $(this.el).children().remove();
+            $(this.el).empty();
             this.render();
-
 
         }
     });
