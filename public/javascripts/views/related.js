3e30a61a6e7c048a27fe1535c238ce137cfff309
Yuval Ziegler <YuvalZiegler@gmail.com>
2012-09-01 23:55:24 -0400

    related works - sort of

--- public/javascripts/views/related.js
+++ public/javascripts/views/related.js
@@ -2,21 +2,35 @@
 var App = (function (App) {
     App.RelatedView = Backbone.View.extend({
         el: '#related',
-
+        events:{
+            "click .tag":          "submitQuery"
+        },
 
         initialize: function (){
-            _.bindAll(this, 'render');
+            _.bindAll(this, 'render', 'submitQuery');
             App.dispatcher.on("chordChange", this.render);
             App.dispatcher.on("scaleChange", this.render);
 
         },
 
         render:function(e){
+           var result,html;
            if (e.isScale) {
-               console.log("chords of scale:", NoteDictionary.getChordsOfScale(e.query));
+               result = NoteDictionary.getChordsOfScale(e.query);
+               html="<h1>"+e.query+" scale includes the following chords:</h1><ul>";
            } else {
-               console.log("chord in scales:", NoteDictionary.getScalesOfChord(e.query));
+               result = NoteDictionary.getScalesOfChord(e.query);
+               html="<h1>the "+e.query+" chord appears in the following scales:</h1><ul>";
            }
+           for (var i=0,l=result.length; i<l; i++){
+
+                html+="<a href='#'><li class='tag'>"+result[i]+"</li></a>";
+           }
+           html+="</ul>";
+           $(this.el).html(html);
+        },
+        submitQuery:function(e){
+           App.notesCollection.setActiveNotes(e.srcElement.innerText);
         }
 
     });
