1c8c3cc9a88bf791f3270c92daf0b2285390acc9
Yuval Ziegler <YuvalZiegler@gmail.com>
2012-09-07 23:09:33 -0400

    fixed test paths

--- test/dictionary.test.js
+++ test/dictionary.test.js
@@ -1,11 +1,9 @@
 require('should');
-//var app = require('../app')
-_ = require('underscore');
-Backbone = require('backbone');
+_ = require('../public/javascripts/libs/underscore.js');
 var dict = require('../public/javascripts/note-dictionary.js');
 
 describe('NoteDictionary', function(){
-  
+
   it('should be an object', function(){
       dict.should.be.a('object');
   });
@@ -25,18 +23,3 @@ describe('NoteDictionary', function(){
 
 
 });
-
-
-/* FUNCTIONS
-• getAllDefinitions: 
-    a list of all the chord and scale names used to populate the autocomplete plugin
-• parseQuery
-    parses a plain english text and calls the appropriate getter (getScale / getChord)
-• getScale
-• getChord
-• getChordsOfScale
-    return all chords in particular scale
-• isChordInScale
-• getScalesOfChord
-    returns all the scale a chord apears in
-*/
