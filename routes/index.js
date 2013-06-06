eb3c0b490f12eddbaaf85a6eeb7d9dfb80dfad6b
Yuval Ziegler <YuvalZiegler@gmail.com>
2013-06-06 11:43:32 -0400

    copy changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@
  * GET home page.
  */
 
-
 exports.index = function(req, res){
     var layout = req.app.settings.env === "development" ? 'layout.jade' : 'layout-min.jade';
     res.render('index', { title: 'Fretboard', layout: layout });
